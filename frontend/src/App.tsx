f57dbe827489761bb07be53bd4cbeb1501090ff7
thanmayisappa <sappathanmayi25@gmail.com>
2026-08-04 14:59:15 +0530

    {create react project}

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>InsightForge AI</h1>
+      <p>Turning Real User Problems into Product Opportunities</p>
+    </div>
+  );
+}
+
+export default App;
